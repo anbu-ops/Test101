070440a507d5df48afca343fdafac8308505fa9c
ANKIT BANERJEE <banerjeeankit71@gmail.com>
2024-09-22 09:10:24 +0530

    fixed the hamburger menu

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class NavigationComponent {
 
+  hamburgerClick(){
+
+    const navbarLinks = document.querySelectorAll('#navbarNav a'); // Selects all anchor tags inside #navbarNav
+
+    console.log(navbarLinks);
+    
+    navbarLinks.forEach((element) => {
+        (element as HTMLElement).style.display = "flex";
+    });
+
+  
+  
+  // toggleButton.addEventListener('click', -> () {
+  //   navbarLinks.classList.toggle('active')
+  // })
+ }
+
 }
